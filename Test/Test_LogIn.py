8c60d370533c9a82142a68f365b5241931b0de49
MarcelloGreen95 <69159494+MarcelloGreen95@users.noreply.github.com>
2021-04-16 17:26:42 +0100

    Added FakeList Test

--- Test/Test_LogIn.py
+++ Test/Test_LogIn.py
@@ -4,21 +4,23 @@ from src.CSVStub import CSVStub
 from src.main import Main
 from src.LogIn import LogIn
 from src.Customers import Customers
-from src.ReadCSVFile import ReadCSVFile
+##from src import ReadCSVFile
 from faker import Faker
 
 
 class Test_LogIn(unittest.TestCase):
     logIn = LogIn()
     main = Main()
-    Customers = Customers()
+    customers = Customers()
+
+    listOfFakeEmails = ['derek.somerville@glasgow.ac.uk', 'matthew.barr@glasgow.ac.uk']
 
     def __init__(self, methodName: str = ...):
         super().__init__(methodName)
         self.readCSVFile = None
 
     def test_CustomerInfo(self):
-        customerInfo = self.readCSVFile.getFiledata("Customer/", "customer.csv")
+        customerInfo = self.readCSVFile.getFileData("Customer/", "customer.csv")
         customerColumns = customerInfo[0]
         self.assertEqual(customerColumns, ["Customers", ])
 
@@ -42,6 +44,12 @@ class Test_LogIn(unittest.TestCase):
         customerData = self.main.property
         customerColumns = customerData[0]
         self.assertEqual(customerColumns, "Email")
+
+    def test_FakeList(self):
+        displayFakeList = LogIn()
+        fakeEmails = Test_LogIn.listOfFakeEmails
+        displayFakeList.getCustomers = MagicMock(return_value=fakeEmails.pop())
+        self.assertEqual('Enter password', displayFakeList.display())
 
 
 if __name__ == '__main__':
